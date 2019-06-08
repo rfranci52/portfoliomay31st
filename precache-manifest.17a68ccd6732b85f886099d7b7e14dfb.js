da7d7c6015acce5fa37862df6feb5bcab9dccd17
Rakim francis <rakimfrancis@Rakims-MBP-2.home>
2019-06-07 22:14:26 -0400

    Updates

---
+++ precache-manifest.17a68ccd6732b85f886099d7b7e14dfb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e0a5325597f1e19c4646c1d3b598d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "257db174958285c9948b",
+    "url": "/static/css/main.3994a1d3.chunk.css"
+  },
+  {
+    "revision": "cca9a102a86c22d67cd2",
+    "url": "/static/js/2.e8e7bf50.chunk.js"
+  },
+  {
+    "revision": "257db174958285c9948b",
+    "url": "/static/js/main.878bbc4f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7c9579e2f310a991ad16eb1677018f1a",
+    "url": "/static/media/click-game.7c9579e2.png"
+  },
+  {
+    "revision": "5e3f88a3c8f602715618267d69ae2d6b",
+    "url": "/static/media/google-books.5e3f88a3.png"
+  },
+  {
+    "revision": "92a510bd40dfb53c52c87ddf6ae2e79a",
+    "url": "/static/media/the-barbershop.92a510bd.png"
+  }
+]);
