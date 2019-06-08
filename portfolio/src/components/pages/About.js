700ab60dde7e235b37e0d3ae7fe442b45f4f29cc
Rakim francis <rakimfrancis@Rakims-MBP-2.home>
2019-06-07 21:25:46 -0400

    added images folder and linked image on portfolio page

--- portfolio/src/components/pages/About.js
+++ portfolio/src/components/pages/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from 'react-bootstrap/Button';
+import image from "../images/click-game.png"
 
 
 const About = (props) => (
@@ -9,38 +10,42 @@ const About = (props) => (
             <h4>Portfolio</h4>
         </div>
         
-        <div class="w3-white w3-margin">
-        <div class="w3-container w3-padding w3-black">
-            <h4>Portfolio</h4>
-        </div></div>
+       
         
         <div class="w3-col s6">
-                <img class="img1" src="https://media.tenor.com/images/973995abc90d4bb0a2b084732201d005/tenor.gif" alt="error"width="200px"/>
+                <img class="img1" src={image}alt="error"width="200px"/>
                 <a class="img1_click" href="hw 6_final/index.html" />
                     <img src="icon.gif" alt=""/>
-                    Giphy API
                 
-                    <a
-          // href="#home"
+                
+           
+        <Button variant="outline-success"rel="noreferrer noopener" target="_blank" href="https://rfranci52.github.io/clickgame/" >React Character Shuffle</Button>
+        <Button variant="outline-success"rel="noreferrer noopener" target="_blank" href="https://github.com/rfranci52/clickgame" >React Character Shuffle Code</Button>
 
-
-
-
-          // onClick={() => props.handlePageChange("Home")}
-          // className={props.currentPage === "Home" ? "nav-link active" : "nav-link"}
-          
-        >
-          Home
-        </a>
-        <Button variant="outline-success" >Success</Button>
-        <a rel="noreferrer noopener" target="_blank" href="http://google.com">
+        {/* <a rel="noreferrer noopener" target="_blank" href="http://google.com">
               Go to recipe!
-            </a>
+            </a> */}
 
         
         
                    
                 {/* </a> */}
+
+
+
+            </div>
+
+            <div class="w3-col s6">
+                <img class="img1" href=".../public/click-game" alt="error"width="200px"/>
+                <a class="img1_click" href="hw 6_final/index.html" />
+                    <img src="icon.gif" alt=""/>
+                
+                
+           
+        <Button variant="outline-success"rel="noreferrer noopener" target="_blank" href="" >React Google Books Search coming soon</Button>
+        <Button variant="outline-success"rel="noreferrer noopener" target="_blank" href="https://github.com/rfranci52/react-google-books" >React Google Books Search Code</Button>
+
+        
 
 
 
